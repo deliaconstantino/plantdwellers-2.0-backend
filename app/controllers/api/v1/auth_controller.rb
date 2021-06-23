b13acf78d7e44624288c93b8a87ca9561689d8d1
deliaconstantino <dconstan92@gmail.com>
2021-06-23 16:06:13 -0400

    implemented backend login with JWT

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::AuthController < ApplicationController
 
   def create
-
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      token = encode_token(user_id: user.id)
+      render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
+    else
+      render json: {message: "invalid login credentials"}, status: :unauthorized
+    end
   end
+
+  private
+    #TODO: santitize incoming input like:
+    # def user_login_params
+    #   # params { user: {username: 'Chandler Bing', password: 'hi' } }
+    #   params.require(:user).permit(:username, :password)
+    # end
+
 end
