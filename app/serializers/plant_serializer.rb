e88bd94535722c12a1d9a505115093102be8615e
deliaconstantino <dconstan92@gmail.com>
2021-07-09 21:12:41 -0400

    updated plant serialzer to render updated column name: 'watering_repeat_rate_days'

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,5 +1,5 @@
 class PlantSerializer
   include JSONAPI::Serializer
-  attributes :common_name, :scientific_name, :location, :watering_rate
+  attributes :common_name, :scientific_name, :location, :watering_repeat_rate_days
   belongs_to :user
 end
