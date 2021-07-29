60bd9351384c1ab6ddecef43b447b0f17b3f9dfd
deliaconstantino <dconstan92@gmail.com>
2021-07-29 11:23:56 -0400

    add presence and uniqueness validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   belongs_to :home, optional: true
   has_many :plants, dependent: :destroy
   has_many :plant_events, through: :plants
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 end
