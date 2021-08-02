1a15742d4d86feb4474702d2ba59fa4be9a4d9c7
deliaconstantino <dconstan92@gmail.com>
2021-08-02 12:40:44 -0400

    add check for whether there is a watering_rate before building plant events for a plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -10,9 +10,11 @@ class Plant < ApplicationRecord
   def build_plant_events_collection(watering_repeat_rate_days, start_date = Date.today)
     event_date = start_date
 
-    while event_date < start_date.at_beginning_of_month.next_month
-      self.plant_events.create(date: event_date, event_type: "water")
-      event_date = event_date.advance(days: watering_repeat_rate_days)
+    if watering_repeat_rate_days
+      while event_date < start_date.at_beginning_of_month.next_month
+        self.plant_events.create(date: event_date, event_type: "water")
+        event_date = event_date.advance(days: watering_repeat_rate_days)
+      end
     end
 
   end
