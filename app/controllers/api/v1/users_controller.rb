ea0132e43fd99fa31f0915a44b0a1004128f24c4
Delia Constantino <dconstan92@gmail.com>
2021-08-09 15:25:38 -0400

    render errors for unsuccessful signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Api::V1::UsersController < ApplicationController
   before_action :authorized, except: [:create]
 
@@ -11,12 +12,11 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-
     if user.save
       token = encode_token(user_id: user.id)
       render json: {user: UserSerializer.new(user), jwt: token}, status: 200
     else
-      render json: {error: "could not save"}, status: 400
+      render json: {errors: user.errors.full_messages}, status: 400
     end
   end
 
